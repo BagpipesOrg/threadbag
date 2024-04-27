8b20d56bddf0edc8d196c36666e6c6c60b7704a2
Filip <flipchan@riseup.net>
2024-04-27 02:33:57 +0200

    transaction que for scenarios

--- src/database/types.rs
+++ src/database/types.rs
@@ -14,6 +14,19 @@ pub struct UrlResponse {
     pub shortUrl: String,
 }
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct TxQueue {
+    pub mempool: Vec<TxInfo>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct TxInfo {
+    pub chain: String,
+    pub amount: String,
+    pub txType: String,
+    pub Date: String,
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct job_start {
     pub scenario_id: String,
