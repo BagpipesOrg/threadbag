68ade5dff82cc9817f4824cc7191e16e151dcbc3
Filip <flipchan@riseup.net>
2024-12-18 14:23:37 +0100

    PoloDB fixes + Error handles

--- src/core/error.rs
+++ src/core/error.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug)]
 pub enum Error {
     /// serde error
@@ -95,6 +97,62 @@ impl From<polodb_core::Error> for Error {
 impl From<std::string::FromUtf8Error> for Error {
     fn from(src: std::string::FromUtf8Error) -> Error {
         Error::Utf8StringError(src)
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::SerdeJson(e) => Some(e),
+            Error::Anyhow(e) => Some(e.as_ref()),
+            Error::SubxtError(e) => Some(e),
+            Error::Polodb(e) => Some(e),
+            Error::HTTPRequestProblem(e) => Some(e),
+            Error::ReqwestError(e) => Some(e),
+            Error::FileIOerror(e) => Some(e),
+            Error::Sled(e) => Some(e),
+            Error::HexError(e) => Some(e),
+            Error::Utf8StringError(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::SerdeJson(e) => write!(f, "Serde JSON error: {}", e),
+            Error::DBwritefail => write!(f, "Could not write to database"),
+            Error::Anyhow(e) => write!(f, "Anyhow error: {}", e),
+            Error::NoEntryInDb => write!(f, "Could not find entry in database"),
+            Error::DecodeProblem => write!(f, "Decode problem"),
+            Error::UnsupportedDestinationChain => write!(f, "Unsupported destination chain"),
+            Error::SubxtError(e) => write!(f, "Subxt error: {}", e),
+            Error::PillDataError => write!(f, "Could not extract data from pill node"),
+            Error::CantFetchWebhook => write!(f, "Cannot fetch webhook data"),
+            Error::ScenarioParseError => write!(f, "Error parsing scenario"),
+            Error::ScenarioIdNotFound => write!(f, "Invalid scenario ID"),
+            Error::Polodb(e) => write!(f, "PoloDB error: {}", e),
+            Error::InvalidDestinationChain => write!(f, "Invalid destination chain"),
+            Error::InvalidChainOption => write!(f, "Invalid chain selected"),
+            Error::CouldNotFindWebhookData => write!(f, "Could not parse webhook UUID"),
+            Error::HTTPRequestProblem(e) => write!(f, "HTTP request problem: {}", e),
+            Error::ReqwestError(e) => write!(f, "Reqwest error: {}", e),
+            Error::Io(msg) => write!(f, "IO error: {}", msg),
+            Error::FileIOerror(e) => write!(f, "File IO error: {}", e),
+            Error::Stderror(msg) => write!(f, "Standard error: {}", msg),
+            Error::STDLIBerror => write!(f, "Standard library error"),
+            Error::ErrorEvent => write!(f, "Error getting block events"),
+            Error::EventNotFound => write!(f, "Could not find event in latest blocks"),
+            Error::StorageItemNotFound => write!(f, "Could not find storage item"),
+            Error::HexError(e) => write!(f, "Hex conversion error: {}", e),
+            Error::MaxConnectionAttemptsExceeded => write!(f, "Max connection attempts exceeded"),
+            Error::ConnectionClosed => write!(f, "Connection to endpoint closed"),
+            Error::CouldNotGetBlock => write!(f, "Could not get requested block"),
+            Error::AsyncNextError => write!(f, "Could not get next item in async loop"),
+            Error::Sled(e) => write!(f, "Sled database error: {}", e),
+            Error::Utf8StringError(e) => write!(f, "UTF-8 string conversion error: {}", e),
+        }
     }
 }
 
